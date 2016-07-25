02f3a8a8e39267e6ea04c5c8538af7cbc16f664e
Rog <Rog@Rogers-Mac.local>
2016-07-25 12:39:24 -0500

    added ui to input data to the table

--- render_projects.rb
+++ render_projects.rb
@@ -19,3 +19,17 @@ SQL
 # create render list table if it doesn't exist already
 db.execute(create_table)
 
+def add_project(db, project_name, type, render_cost, est_time_completion)
+	db.execute("INSERT INTO render_list(project_name, type, render_cost,est_time_completion) VALUES(?, ?, ?, ?)",[project_name, type, render_cost,est_time_completion])
+end
+
+puts "What is the new project name?"
+project_name = gets.chomp
+puts "What type of project is it?"
+type = gets.chomp
+puts "What will be the render price?"
+render_cost = gets.chomp.to_i
+puts "What is the estimated time for compeltion? (days)"
+est_time_completion = gets.chomp.to_i
+
+add_project(db, project_name, type, render_cost, est_time_completion)
